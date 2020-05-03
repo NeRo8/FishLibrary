ed707fb6c622bb4b398246888184b45664fc6755
Nero <arthur.duchenko@gmail.com>
2020-05-03 21:57:52 +0300

    Add index request

--- fishLibrary/urls.py
+++ fishLibrary/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('library/', include('fishingLibrary.urls')),
+    path('library/', include('fishingLibrary.urls')),
     path('admin/', admin.site.urls),
 ]
